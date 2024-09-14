9b98b95079b6f657fa7555ccda5bc667fbae1d35
dwl21 <nggus5@gmail.com>
2024-09-14 14:43:41 +0900

    chore: 테스트 설정 파일

---
+++ src/test/java/com/yourssu/blog/support/acceptance/AcceptanceTest.java
@@ -0,0 +1,24 @@
+package com.yourssu.blog.support.acceptance;
+
+import com.yourssu.blog.support.common.DatabaseCleaner;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AcceptanceTest {
+
+    @LocalServerPort
+    protected int port;
+
+    @Autowired
+    private DatabaseCleaner databaseCleaner;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+        databaseCleaner.execute();
+    }
+}
