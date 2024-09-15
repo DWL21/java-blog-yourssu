55762f4c4c846663f3907acccb85f58d3695cb23
dwl21 <nggus5@gmail.com>
2024-09-15 12:15:03 +0900

    feat: 댓글 수정 메서드 구현

--- src/test/java/com/yourssu/blog/support/common/fixture/CommentFixture.java
+++ src/test/java/com/yourssu/blog/support/common/fixture/CommentFixture.java
@@ -1,14 +1,19 @@
 package com.yourssu.blog.support.common.fixture;
 
 import com.yourssu.blog.controller.dto.CommentCreateRequest;
+import com.yourssu.blog.controller.dto.CommentEditRequest;
 import com.yourssu.blog.model.Article;
 import com.yourssu.blog.model.Comment;
+import com.yourssu.blog.service.dto.CommentRequest;
 import com.yourssu.blog.service.dto.CommentSaveRequest;
+import com.yourssu.blog.service.dto.CommentUpdateRequest;
 
 public enum CommentFixture {
 
     LEO(UserFixture.LEO,
-            "leo comment");
+            "leo comment"),
+    EVOLVED_LEO(UserFixture.LEO,
+            "leo evolved comment");
 
     private final UserFixture userFixture;
     private final String content;
@@ -28,5 +33,17 @@ public enum CommentFixture {
 
     public CommentSaveRequest getCommentSaveRequest(Article article) {
         return new CommentSaveRequest(article.getArticleId(), userFixture.getEmail(), userFixture.getPassword(), content);
+    }
+
+    public CommentEditRequest getCommentEditRequest() {
+        return new CommentEditRequest(userFixture.getEmail(), userFixture.getPassword(), content);
+    }
+
+    public CommentUpdateRequest getCommentUpdateRequest(Article article, Long commentId) {
+        return new CommentUpdateRequest(
+                new CommentRequest(article.getArticleId(), commentId),
+                userFixture.getEmail(),
+                userFixture.getPassword(),
+                content);
     }
 }
