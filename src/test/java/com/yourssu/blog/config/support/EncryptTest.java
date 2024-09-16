a0c96cdb01e55171b032ef56a771d19a959f9d09
dwl21 <nggus5@gmail.com>
2024-09-16 16:08:58 +0900

    feat: SHA-256 해시 알고리즘을 사용한 암호화 구현

---
+++ src/test/java/com/yourssu/blog/config/support/EncryptTest.java
@@ -0,0 +1,38 @@
+package com.yourssu.blog.config.support;
+
+import com.yourssu.blog.support.service.ApplicationTest;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+
+@ApplicationTest
+class EncryptTest {
+
+    @Autowired
+    private Encrypt encrypt;
+
+    @Test
+    @DisplayName("문자열을 SHA-256 해시 알고리즘으로 암호화한다.")
+    void encrypt() {
+        String password = "yourssu2024";
+
+        assertThatNoException().isThrownBy(
+                () -> encrypt.encrypt(password));
+    }
+
+    @Test
+    @Disabled
+    @DisplayName("실제로 문자열을 SHA-256 해시 알고리즘으로 암호화한다. Salt가 변경되면 통과하지 않는다.")
+    void encryptWithSalt() {
+        String password = "yourssu2024";
+
+        String actual = encrypt.encrypt(password);
+
+        String expected = "9b09128cf76b491c12c57c796a87b56f71a207d8ccb2b5911c7c2029c6a6f150";
+        assertThat(actual).isEqualTo(expected);
+    }
+}
