29e2abf2c1ddf5709a264b0887377f84adc6edd6
dwl21 <nggus5@gmail.com>
2024-09-16 15:35:25 +0900

    feat: 회원 엔티티 구현

---
+++ src/main/java/com/yourssu/blog/model/User.java
@@ -0,0 +1,38 @@
+package com.yourssu.blog.model;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "\"user\"")
+@Getter
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "passsword", nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String username;
+
+    public User(String email, String password, String username) {
+        this(null, email, password, username);
+    }
+
+    public User(Long id, String email, String password, String username) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.username = username;
+    }
+}
---
+++ src/test/java/com/yourssu/blog/model/repository/UserRepositoryTest.java
@@ -0,0 +1,40 @@
+package com.yourssu.blog.model.repository;
+
+import com.yourssu.blog.support.common.fixture.UserFixture;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThatNoException;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    @DisplayName("회원 번호와 일치하는 회원을 반환한다.")
+    void findById() {
+        UserFixture user = UserFixture.LEO;
+        userRepository.save(user.getUser());
+
+        assertThatNoException().isThrownBy(
+                () -> userRepository.get(1L)
+        );
+    }
+
+    @Test
+    @DisplayName("이메일과 일치하는 회원을 반환한다.")
+    void findByEmail() {
+        UserFixture user = UserFixture.LEO;
+        userRepository.save(user.getUser());
+
+        assertThatNoException().isThrownBy(
+                () -> userRepository.getByEmail(user.getEmail())
+        );
+    }
+}
