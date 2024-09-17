ce4c085858b807c66c10737d9e03946b0bb72d50
dwl21 <nggus5@gmail.com>
2024-09-17 12:32:28 +0900

    test: 회원 조회 메서드 테스트 일반화

--- src/test/java/com/yourssu/blog/model/repository/UserRepositoryTest.java
+++ src/test/java/com/yourssu/blog/model/repository/UserRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.yourssu.blog.model.repository;
 
+import com.yourssu.blog.model.User;
 import com.yourssu.blog.support.common.fixture.UserFixture;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 
 @DataJpaTest
@@ -20,11 +22,11 @@ class UserRepositoryTest {
     @DisplayName("회원 번호와 일치하는 회원을 반환한다.")
     void findById() {
         UserFixture user = UserFixture.LEO;
-        userRepository.save(user.getUser());
+        User expected = userRepository.save(user.getUser());
 
-        assertThatNoException().isThrownBy(
-                () -> userRepository.get(1L)
-        );
+        User actual = userRepository.get(expected.getId());
+
+        assertThat(actual).isEqualTo(expected);
     }
 
     @Test
