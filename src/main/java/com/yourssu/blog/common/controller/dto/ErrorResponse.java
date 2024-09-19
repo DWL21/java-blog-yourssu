ef86f312f2de8e74ee05f12eb6fc468eef35e6a0
dwl21 <nggus5@gmail.com>
2024-09-19 11:54:45 +0900

    feat: ErrorResponse 현재 시간도 반환

--- src/main/java/com/yourssu/blog/common/controller/dto/ErrorResponse.java
+++ src/main/java/com/yourssu/blog/common/controller/dto/ErrorResponse.java
@@ -1,4 +1,17 @@
 package com.yourssu.blog.common.controller.dto;
 
-public record ErrorResponse(String message){
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class ErrorResponse {
+
+    private final LocalDateTime time;
+    private final String message;
+
+    public ErrorResponse(String message) {
+        this.time = LocalDateTime.now();
+        this.message = message;
+    }
 }
