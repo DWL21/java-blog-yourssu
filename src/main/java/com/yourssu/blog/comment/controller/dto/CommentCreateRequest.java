cc695ad1cff9efaae42dcaad3773a3a5ee1bdcf0
dwl21 <nggus5@gmail.com>
2024-09-19 13:22:02 +0900

    feat: 댓글 작성 예외 처리

--- src/main/java/com/yourssu/blog/comment/controller/dto/CommentCreateRequest.java
+++ src/main/java/com/yourssu/blog/comment/controller/dto/CommentCreateRequest.java
@@ -1,8 +1,13 @@
 package com.yourssu.blog.comment.controller.dto;
 
 import com.yourssu.blog.comment.service.dto.CommentSaveRequest;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
-public record CommentCreateRequest(String content) {
+public record CommentCreateRequest(
+        @NotBlank(message = "댓글 입력이 올바르지 않습니다.")
+        @Size(max = 255, message = "댓글 입력이 255자를 초과할 수 없습니다.")
+        String content) {
 
     public CommentSaveRequest toCommentSaveRequest(Long articleId, Long userId) {
         return new CommentSaveRequest(articleId, userId, content);
