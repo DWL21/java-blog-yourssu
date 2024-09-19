7cc2582fb66f96ae603c39e8d42ac19eb793eb72
dwl21 <nggus5@gmail.com>
2024-09-19 12:59:11 +0900

    feat: 게시글 작성 예외 처리

--- src/main/java/com/yourssu/blog/article/controller/dto/ArticleCreateRequest.java
+++ src/main/java/com/yourssu/blog/article/controller/dto/ArticleCreateRequest.java
@@ -1,8 +1,17 @@
 package com.yourssu.blog.article.controller.dto;
 
 import com.yourssu.blog.article.service.dto.ArticleSaveRequest;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
-public record ArticleCreateRequest(String title, String content) {
+public record ArticleCreateRequest(
+        @NotBlank(message = "제목 입력이 올바르지 않습니다.")
+        @Size(max = 255, message = "제목 입력이 255자를 초과할 수 없습니다.")
+        String title,
+
+        @NotBlank(message = "본문 입력이 올바르지 않습니다.")
+        @Size(max = 255, message = "본문 입력이 255자를 초과할 수 없습니다.")
+        String content) {
 
     public ArticleSaveRequest toArticleSaveRequest(Long userId) {
         return new ArticleSaveRequest(userId, title, content);
