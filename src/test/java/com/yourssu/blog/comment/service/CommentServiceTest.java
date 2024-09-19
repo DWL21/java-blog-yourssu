4bd5d47f16a6e44b321466b99f146062b59a4415
dwl21 <nggus5@gmail.com>
2024-09-19 17:25:03 +0900

    test: 댓글 기능 예외 서비스 테스트 작성

--- src/test/java/com/yourssu/blog/comment/service/CommentServiceTest.java
+++ src/test/java/com/yourssu/blog/comment/service/CommentServiceTest.java
@@ -5,12 +5,16 @@ import com.yourssu.blog.comment.service.dto.CommentDeleteRequest;
 import com.yourssu.blog.comment.service.dto.CommentResponse;
 import com.yourssu.blog.comment.service.dto.CommentSaveRequest;
 import com.yourssu.blog.comment.service.dto.CommentUpdateRequest;
+import com.yourssu.blog.common.exception.ForbiddenException;
+import com.yourssu.blog.common.exception.InvalidRequestException;
+import com.yourssu.blog.common.exception.NotFoundException;
 import com.yourssu.blog.user.model.User;
 import com.yourssu.blog.article.model.repository.ArticleRepository;
 import com.yourssu.blog.user.model.repository.UserRepository;
 import com.yourssu.blog.support.common.fixture.ArticleFixture;
 import com.yourssu.blog.support.common.fixture.UserFixture;
 import com.yourssu.blog.support.service.ApplicationTest;
+import org.hibernate.annotations.NotFoundAction;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +22,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import static com.yourssu.blog.support.common.fixture.CommentFixture.EVOLVED_LEO;
 import static com.yourssu.blog.support.common.fixture.CommentFixture.LEO;
 import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @ApplicationTest
 class CommentServiceTest {
@@ -36,7 +41,7 @@ class CommentServiceTest {
     void save() {
         User user = saveUser(UserFixture.LEO);
         Article article = saveArticle(ArticleFixture.LEO, user);
-        CommentSaveRequest request = LEO.getCommentSaveRequest(article, user.getId());
+        CommentSaveRequest request = LEO.getCommentSaveRequest(article.getId(), user.getId());
 
         assertThatNoException().isThrownBy(
                 () -> commentService.save(request)
@@ -44,11 +49,21 @@ class CommentServiceTest {
     }
 
     @Test
+    @DisplayName("존재하지 않는 게시글 번호일 경우 예외를 반환한다.")
+    void saveWhenNotFoundArticle() {
+        User user = saveUser(UserFixture.LEO);
+        CommentSaveRequest request = LEO.getCommentSaveRequest(1L, user.getId());
+
+        assertThatThrownBy(() -> commentService.save(request))
+                .isInstanceOf(NotFoundException.class);
+    }
+
+    @Test
     @DisplayName("댓글을 수정한다.")
     void update() {
         User user = saveUser(UserFixture.LEO);
         Article article = saveArticle(ArticleFixture.LEO, user);
-        CommentResponse given = commentService.save(LEO.getCommentSaveRequest(article, user.getId()));
+        CommentResponse given = commentService.save(LEO.getCommentSaveRequest(article.getId(), user.getId()));
 
         CommentUpdateRequest request = EVOLVED_LEO.getCommentUpdateRequest(article, given.getCommentId(), user.getId());
 
@@ -58,11 +73,52 @@ class CommentServiceTest {
     }
 
     @Test
+    @DisplayName("존재하지 않는 댓글 번호일 경우 예외를 반환한다.")
+    void updateWhenNotFoundComment() {
+        User user = saveUser(UserFixture.LEO);
+        Article article = saveArticle(ArticleFixture.LEO, user);
+
+        CommentUpdateRequest request = EVOLVED_LEO.getCommentUpdateRequest(article, 1L, user.getId());
+
+        assertThatThrownBy(() -> commentService.update(request))
+                .isInstanceOf(NotFoundException.class);
+    }
+
+    @Test
+    @DisplayName("게시글 번호에 해당하지 않는 댓글 번호일 경우 예외를 반환한다.")
+    void updateWhenInvalidRequest() {
+        User user = saveUser(UserFixture.LEO);
+        Article article = saveArticle(ArticleFixture.LEO, user);
+        CommentResponse comment = commentService.save(LEO.getCommentSaveRequest(article.getId(), user.getId()));
+        Article anotherArticle = saveArticle(ArticleFixture.LEO, user);
+
+        CommentUpdateRequest request = EVOLVED_LEO.getCommentUpdateRequest(anotherArticle, comment.getCommentId(), user.getId());
+
+        assertThatThrownBy(() -> commentService.update(request))
+                .isInstanceOf(InvalidRequestException.class);
+    }
+
+    @Test
+    @DisplayName("댓글 작성자의 요청이 아닌 경우 예외를 반환한다.")
+    void updateWhenForbidden() {
+        User user = saveUser(UserFixture.LEO);
+        Article article = saveArticle(ArticleFixture.LEO, user);
+        User anotherUser = saveUser(UserFixture.MIO);
+
+        CommentResponse given = commentService.save(LEO.getCommentSaveRequest(article.getId(), user.getId()));
+
+        CommentUpdateRequest request = EVOLVED_LEO.getCommentUpdateRequest(article, given.getCommentId(), anotherUser.getId());
+
+        assertThatThrownBy(() -> commentService.update(request))
+                .isInstanceOf(ForbiddenException.class);
+    }
+
+    @Test
     @DisplayName("댓글을 삭제한다.")
     void delete() {
         User user = saveUser(UserFixture.LEO);
         Article article = saveArticle(ArticleFixture.LEO, user);
-        CommentResponse given = commentService.save(LEO.getCommentSaveRequest(article, user.getId()));
+        CommentResponse given = commentService.save(LEO.getCommentSaveRequest(article.getId(), user.getId()));
 
         CommentDeleteRequest request = LEO.getCommentDeleteRequest(article, given.getCommentId(), user.getId());
 
@@ -70,6 +126,48 @@ class CommentServiceTest {
                 () -> commentService.delete(request)
         );
     }
+
+    @Test
+    @DisplayName("존재하지 않는 댓글 번호일 경우 예외를 반환한다.")
+    void deleteWhenNotFoundComment() {
+        User user = saveUser(UserFixture.LEO);
+        Article article = saveArticle(ArticleFixture.LEO, user);
+
+        CommentDeleteRequest request = EVOLVED_LEO.getCommentDeleteRequest(article, 1L, user.getId());
+
+        assertThatThrownBy(() -> commentService.delete(request))
+                .isInstanceOf(NotFoundException.class);
+    }
+
+    @Test
+    @DisplayName("게시글 번호에 해당하지 않는 댓글 번호일 경우 예외를 반환한다.")
+    void deleteWhenInvalidRequest() {
+        User user = saveUser(UserFixture.LEO);
+        Article article = saveArticle(ArticleFixture.LEO, user);
+        CommentResponse comment = commentService.save(LEO.getCommentSaveRequest(article.getId(), user.getId()));
+        Article anotherArticle = saveArticle(ArticleFixture.LEO, user);
+
+        CommentDeleteRequest request = EVOLVED_LEO.getCommentDeleteRequest(anotherArticle, comment.getCommentId(), user.getId());
+
+        assertThatThrownBy(() -> commentService.delete(request))
+                .isInstanceOf(InvalidRequestException.class);
+    }
+
+    @Test
+    @DisplayName("댓글 작성자의 요청이 아닌 경우 예외를 반환한다.")
+    void deleteWhenForbidden() {
+        User user = saveUser(UserFixture.LEO);
+        Article article = saveArticle(ArticleFixture.LEO, user);
+        User anotherUser = saveUser(UserFixture.MIO);
+
+        CommentResponse given = commentService.save(LEO.getCommentSaveRequest(article.getId(), user.getId()));
+
+        CommentDeleteRequest request = EVOLVED_LEO.getCommentDeleteRequest(article, given.getCommentId(), anotherUser.getId());
+
+        assertThatThrownBy(() -> commentService.delete(request))
+                .isInstanceOf(ForbiddenException.class);
+    }
+
 
     private User saveUser(UserFixture user) {
         return userRepository.save(user.getUser());
